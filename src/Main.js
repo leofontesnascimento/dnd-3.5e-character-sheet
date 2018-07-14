6456e9fac660e908ce6bacef6cd2bdbaef822a4a
Leonardo Nascimento <lnascimento.mail@gmail.com>
2018-07-14 11:53:09 -0400

    Wrap the app with ract redux provider
    Also it was added the store and the combine reducers

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+
+import { createStore } from 'redux';
+
+import reducers from './reducers';
+
 import DrawerNavigator from './DrawerNavigator';
 
 export default props => (
-  <DrawerNavigator />
+  <Provider store={createStore(reducers)}>
+    <DrawerNavigator />
+  </Provider>
 );
